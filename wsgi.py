0a0c4ef42ff27acc3cde97790aa818db49ac28f2
jokuttan <joeltabraham@gmail.com>
2018-02-25 12:56:01 +0000

    added the programs

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return ["<h1 style='color:blue'>Hello There!</h1>"]
+print("Joel here now.")
+print("Joel here now.")
+print("Joel here now.")
+
